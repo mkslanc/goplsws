5ba129086fdad1e38595019cad6c09698ed6bc0b
mkslanc <mkslanc@gmail.com>
2024-04-23 18:14:05 +0400

    add go/golang replacement

--- paths-utility.js
+++ paths-utility.js
@@ -2,11 +2,11 @@ const path = require("path");
 const {URI} = require("vscode-uri");
 
 function makeServerPath(fileName) {
-    return formatPath(__dirname + path.sep + "temp" + path.sep + fileName);
+    return formatPath(__dirname + path.sep + "temp" + path.sep + fileName).replace(/.golang$/, ".go");
 }
 
 function makeClientPath(filePath) {
-    return filePath.split(/[/\\]/).pop();
+    return filePath.split(/[/\\]/).pop().replace(/.go$/, ".golang");
 }
 
 function formatPath(filePath) {
